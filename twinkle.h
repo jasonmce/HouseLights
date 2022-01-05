63bdfaf24e3e5c69165c6ab509e57119350b8d83
McEachen, Jason <mceachenj@hhmi.org>
2022-01-04 19:03:22 -0500

    Improve toggleTwinkles params

--- twinkle.h
+++ twinkle.h
@@ -4,32 +4,38 @@ int twinkle[2][50];
 int twinkle_step = -1;
 
 /**
- * Randomly picks 5% of the strip to blink on and off.
+ * Randomly picks {percent_lit}% of the strip to blink on and off.
  * 
  * Assumes odd lights are available to twinkle.
+ * 
+ * @param Adafruit_NeoPixel *lights
+ *   Adafruit_NeoPixel pointer for performing light counting and changing.
+ * @param int percent_lit
+ *   Number between 0-100 to determine how many bulbs to twinkle.
  */
-void toggleTwinkles(Adafruit_NeoPixel *my_strip) {
-  int half_string_length = my_strip->numPixels() / 2;
-  uint32_t color_white = my_strip->Color(255, 255, 255);
-  uint32_t color_black = my_strip->Color(0, 0, 0);
+void toggleTwinkles(Adafruit_NeoPixel *lights, int percent_lit) {
+  uint32_t color_white = lights->Color(255, 255, 255);
+  uint32_t color_black = lights->Color(0, 0, 0);
 
+  int num_lit = (lights->numPixels() * percent_lit) / 100;
+  int half_string_length = lights->numPixels() / 2;
 
   if (-1 == twinkle_step) {
-    for (int count = 0; count < (half_string_length * 0.5); count++) {
+    for (int count = 0; count < num_lit; count++) {
       twinkle[1][count] = random(0, half_string_length) * 2 + 1;
-      my_strip->setPixelColor(twinkle[1][count], color_white);
+      lights->setPixelColor(twinkle[1][count], color_white);
       twinkle[0][count] = random(0, half_string_length) * 2 + 1;
-      my_strip->setPixelColor(twinkle[0][count], color_white);
+      lights->setPixelColor(twinkle[0][count], color_white);
     }
     twinkle_step = 0;
     return;
   }
 
-  for (int count = 0; count < (half_string_length * 0.5); count++) {
-    my_strip->setPixelColor(twinkle[twinkle_step][count], color_black);
+  for (int count = 0; count < num_lit; count++) {
+    lights->setPixelColor(twinkle[twinkle_step][count], color_black);
     twinkle[twinkle_step][count] = random(0, half_string_length) * 2 + 1;
-    my_strip->setPixelColor(twinkle[twinkle_step][count], color_white);
+    lights->setPixelColor(twinkle[twinkle_step][count], color_white);
   }
   twinkle_step = (twinkle_step + 1) % 2;
-  my_strip->show();
+  lights->show();
 }
