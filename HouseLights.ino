8492685b6e1528e4014205412d599ee6b8982f38
JasonMcE <bitbucket@brightshinyobject.com>
2024-03-14 10:43:26 -0400

    Reduce number of lights to match what is in use.  Switch palette

--- HouseLights.ino
+++ HouseLights.ino
@@ -21,7 +21,7 @@
 #include "ntp.h"
 
 // Use NEO_GBR for the pixel ring to test
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(400, 2, NEO_RGB + NEO_KHZ400);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(300, 2, NEO_RGB + NEO_KHZ400);
 
 uint32_t BLACK = strip.Color(0, 0, 0);
 uint32_t WHITE = strip.Color(255, 255, 255);
@@ -41,55 +41,92 @@ int super_counter = 0;
 int loop_counter = 0;
 int current_effect_index = 0;
 
-uint32_t color_palette[7] = {RED, BLUE, ORANGE, INDIGO, YELLOW, ROSE, GREEN};
+//uint32_t color_palette[7] = {RED, BLUE, ORANGE, INDIGO, YELLOW, ROSE, GREEN};
 //uint32_t ukraine_palette[2] = {BLUE, YELLOW};
-uint32_t ukraine_palette[2] = {BLUE, strip.Color(153, 153, 15)};
-uint32_t ukraine_palette_with_spacing[4] = {BLUE, YELLOW, BLACK, BLACK};
-uint32_t voilets_palette[] = {BLUE, INDIGO, VIOLET, ROSE, WHITE};
-uint32_t rouges_palette[] = {RED, INDIGO, VIOLET, ROSE, WHITE};
-uint32_t chinese_palette[] = {RED, GOLD, ORANGE, YELLOW};
-uint32_t chiefs_palette[] = {RED, GOLD, RED, WHITE};
+uint32_t wider_ukraine_palette[6] = {BLUE, BLUE, BLUE, YELLOW, YELLOW, YELLOW};
 
-uint32_t halloween_palette[4] = {  
-  strip.Color(0, 101, 0), // green
-  strip.Color(31, 0, 67), // purple
-  strip.Color(151, 67, 0), // orange
-  strip.Color(151, 122, 0), // yellow
-};
-// From https://looka.com/blog/12-fall-color-palettes/ &. Pumpkin spice
-uint32_t fall_palette[5] = {  
-  strip.Color(89, 50, 10), // Chocolate brown
-  strip.Color(153, 73, 0), // light brown
-  strip.Color(196, 84, 0), // ochre
-  strip.Color(209, 114, 0), // yellow ochre
-  strip.Color(240, 227, 214), // light pinkish?
-};
-uint32_t pinks_palette[8] = {
-  strip.Color(255, 0, 0),
-  strip.Color(255, 50, 50),
-  strip.Color(255, 100, 100),
-  strip.Color(255, 125, 125),
-  strip.Color(255, 150, 150),
-  strip.Color(255, 200, 200),
-  strip.Color(255, 255, 255),
-  strip.Color(255, 255, 255),
-  };
-uint32_t irish_flag_palette[3] = {
-  strip.Color(22, 155, 98),
-  strip.Color(255, 255, 255),
-  strip.Color(255, 136, 62),
-};
-uint32_t christmas_palette[5] = {
-  RED,
-  GREEN,
-  INDIGO,
-  GOLD,
-  VIOLET
-};
-uint32_t blue_and_white[2] = {
-  BLUE,
-  WHITE,
-};
+//uint32_t ukraine_palette[2] = {BLUE, strip.Color(153, 153, 15)};
+//
+//uint32_t ukraine_usa_palette[24] = {
+//  BLUE, strip.Color(153, 153, 15),
+//  BLUE, strip.Color(153, 153, 15),
+//  BLUE, strip.Color(153, 153, 15),
+//  BLUE, strip.Color(153, 153, 15),
+//  BLUE, strip.Color(153, 153, 15),
+//  BLUE, strip.Color(153, 153, 15),
+//  BLUE, strip.Color(153, 153, 15),
+//  BLUE, strip.Color(153, 153, 15),
+//  RED, RED, RED, WHITE, WHITE, WHITE, BLUE, BLUE   
+//};
+//
+//uint32_t diwali_palette[4] = {
+//  GOLD,
+//  RED,
+//  YELLOW,
+//  ORANGE,
+//};
+//
+// uint32_t ukraine_palette_with_spacing[4] = {BLUE, YELLOW, BLACK, BLACK};
+//uint32_t voilets_palette[] = {BLUE, INDIGO, VIOLET, ROSE, WHITE};
+//uint32_t rouges_palette[] = {RED, INDIGO, VIOLET, ROSE, WHITE};
+//uint32_t chinese_palette[] = {RED, GOLD, ORANGE, YELLOW};
+// uint32_t chiefs_wide_palette[] = {RED, RED, RED, GOLD, GOLD, GOLD, RED, RED, RED, WHITE, WHITE, WHITE};
+// uint32_t chiefs_palette[2] = {RED, GOLD};
+//
+//uint32_t halloween_palette[4] = {  
+//  strip.Color(0, 101, 0), // green
+//  strip.Color(31, 0, 67), // purple
+//  strip.Color(151, 67, 0), // orange
+//  strip.Color(151, 122, 0), // yellow
+//};
+//// From https://looka.com/blog/12-fall-color-palettes/ &. Pumpkin spice
+//uint32_t fall_palette[5] = {  
+//  strip.Color(89, 50, 10), // Chocolate brown
+//  strip.Color(153, 73, 0), // light brown
+//  strip.Color(196, 84, 0), // ochre
+//  strip.Color(209, 114, 0), // yellow ochre
+//  strip.Color(240, 227, 214), // light pinkish?
+//};
+//uint32_t pinks_palette[8] = {
+//  strip.Color(255, 0, 0),
+//  strip.Color(255, 50, 50),
+//  strip.Color(255, 100, 100),
+//  strip.Color(255, 125, 125),
+//  strip.Color(255, 150, 150),
+//  strip.Color(255, 200, 200),
+//  strip.Color(255, 255, 255),
+//  strip.Color(255, 255, 255),
+//  };
+//uint32_t irish_flag_palette[3] = {
+//  strip.Color(22, 155, 98),
+//  strip.Color(255, 255, 255),
+//  strip.Color(255, 136, 62),
+//};
+//uint32_t st_pats_palette[] = {
+//  GREEN, GREEN, GREEN, GREEN,
+//  GOLD, GOLD, GOLD, GOLD, 
+//  GREEN, GREEN, GREEN, GREEN,
+//  WHITE, WHITE, WHITE, WHITE,
+//};
+
+// uint32_t christmas_palette[6] = {
+//   RED,
+//   GREEN,
+//   WHITE,
+//   INDIGO,
+//   GOLD,
+//   VIOLET
+// };
+
+// uint32_t blue_and_white[2] = {
+//  BLUE,
+//  WHITE,
+// };
+
+//uint32_t orange_purple[2] = {
+//  strip.Color(31, 0, 67), // purple
+//  strip.Color(151, 67, 0), // orange
+//};
 
 // Start at zero so it first runs at boot.
 int duskCheckCountdown = 0;
@@ -106,7 +143,7 @@ void setup() {
   delay(500);
   Serial.println("setup()");
   strip.setBrightness(32);
-  strip.fill(BLACK);
+  strip.clear();
   strip.show();
   
 //  Sprites* sprites = new Sprites();
@@ -116,14 +153,13 @@ void setup() {
 //  current_effect = sprites;
 
 
-//  Morse* morse = new Morse();
-//  morse->setPalette(rouges_palette, 5);
-//  current_effect = morse;
+  Morse* morse = new Morse();
+  morse->setPalette(wider_ukraine_palette, sizeof(wider_ukraine_palette) / sizeof(uint32_t));
+  current_effect = morse;
 
-
-  Patterns::WithSpacing(&strip, color_palette, sizeof(color_palette) / sizeof(uint32_t), 1);
-  ColorParade* colorParade = new ColorParade(chiefs_palette, 4, 12);
-  current_effect = colorParade;
+//  Patterns::WithSpacing(&strip, color_palette, sizeof(color_palette) / sizeof(uint32_t), 1);
+//  ColorParade* colorParade = new ColorParade(blue_and_white, sizeof(blue_and_white) / sizeof(uint32_t), 12);
+//  current_effect = colorParade;
 
   current_effect->setup(&strip);
 }
